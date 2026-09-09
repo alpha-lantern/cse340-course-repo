8566688fac48b2f4698ff0777dae031eaa98530a
Alvin Timana <alvinjoelts@gmail.com>
2026-09-09 11:04:07 -0500

    Added data base parse function to retrieve the data and display it in organizations page

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { fileURLToPath } from 'url';
 import path from 'path';
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 // Define the application environment
 const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
@@ -37,9 +39,12 @@ app.get('/', async (req, res) => {
 });
 
 app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    // console.log('Retrieved organizations:', organizations);
+
     const title = 'Our Partner Organizations';
     const description = 'Discover our network of partner organizations';
-    res.render('organizations', { title, description });
+    res.render('organizations', { title, description, organizations });
 });
 
 app.get('/projects', async (req, res) => {
@@ -54,7 +59,12 @@ app.get('/categories', async (req, res) => {
     res.render('categories', { title, description });
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running at http://127.0.0.1:${PORT}`);
-    console.log(`Environment: ${NODE_ENV}`);
+app.listen(PORT, async () => {
+    try {
+        await testConnection();
+        console.log(`Server is running at http://127.0.0.1:${PORT}`);
+        console.log(`Environment: ${NODE_ENV}`);
+    } catch (error) {
+        console.error('Error connecting to the database:', error);
+    }
 });
